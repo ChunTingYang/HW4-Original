f2adaa1c6a0437667952ede6bb59bba02aa1f9f8
Chun-Ting Yang <chun-ting-yang@uiowa.edu>
2015-10-16 05:46:04 -0500

    Modified ReadQuery.java

--- src/java/dbHelpers/ReadQuery.java
+++ src/java/dbHelpers/ReadQuery.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.Actors;
 
 public class ReadQuery {
     
@@ -45,4 +47,66 @@ public class ReadQuery {
             Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    public void doRead(){
+        
+        try {
+            String query = "Select * from friends";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            this.results = ps.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public String getHTMLtable(){
+        
+        String table = "";
+        table += "<table border=1>";
+        
+        try {
+            while(this.results.next()){
+                
+                Actors actor = new Actors();
+                actor.setActorID(this.results.getInt("actorID"));
+                actor.setActorName(this.results.getString("actorName"));
+                actor.setNationality(this.results.getString("nationality"));
+                actor.setAge(this.results.getInt("age"));
+                actor.setBirthday(this.results.getString("birthday"));
+                
+                table += "<tr>";
+                table += "<td>";
+                table += actor.getActorID();
+                table += "</td>";
+                
+                table += "<td>";
+                table += actor.getActorName();
+                table += "</td>";
+                
+                table += "<td>";
+                table += actor.getNationality();
+                table += "</td>";
+                
+                table += "<td>";
+                table += actor.getAge();
+                table += "</td>";
+                
+                table += "<td>";
+                table += actor.getBirthday();
+                table += "</td>";
+                
+                table += "</tr>";
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ReadQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        table += "</table>";
+                
+                return table;
+    
+    
+    }
+    
 }
